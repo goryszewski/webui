ca619e2a1616177aeede57809fe3c3c551919fea
Michal Goryszewski <goryszewski@gmail.com>
2023-12-31 22:40:44 +0100

    Add env variable

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    link: 'http://127.0.0.1:5000',
+    title: 'dev environment',
+    code: 1004,
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    link: 'https://api.prod.auto8ks.etc',
+    title: 'production environment',
+    code: 1004,
+};
